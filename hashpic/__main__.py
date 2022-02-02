b0df6724e33560713742f84ef35a273d9191581f
0xflotus <0xflotus@gmail.com>
2022-02-02 15:41:40 +0100

    sha512 support

--- hashpic/__main__.py
+++ hashpic/__main__.py
@@ -1,10 +1,6 @@
 import argparse
-from PIL import Image, ImageOps
-import os
-import hashlib
-import sys
-import re
-from .util import *
+from .md5 import *
+from .sha512 import *
 
 
 def main():
@@ -12,102 +8,15 @@ def main():
     parser.add_argument("input", nargs="*", action="store", help="Input string to hash")
     parser.add_argument("-d", action="store_true", help="debug mode")
     parser.add_argument("-i", action="store_true", help="invert the image")
-    parser.add_argument("--md5", action="store_true", help="give an md5 hash directly")
+    parser.add_argument("--bypass", action="store_true", help="give a hash directly")
     parser.add_argument("-c", action="store_true", help="console mode")
+    parser.add_argument("--sha512", action="store_true")
     args = parser.parse_args()
 
-    if not args.input:
-        hash = (
-            hashlib.md5(sys.stdin.read().encode()).hexdigest()
-            if not args.md5
-            else sys.stdin.read().rstrip("\n").lower()
-        )
+    if args.sha512:
+        sha_512_mode(input=args.input, bypass=args.bypass, debug=args.d, console=args.c, invert=args.i)
     else:
-        hash = (
-            hashlib.md5(" ".join(args.input).encode()).hexdigest()
-            if not args.md5
-            else args.input.lower()
-        )
-
-    pattern = re.compile(r"^[a-f0-9]{32}$")
-    match = pattern.match(hash)
-    if not match:
-        sys.stderr.write(f"{hash} is not a valid MD5 hash\n")
-        sys.exit(-1)
-
-    if args.d:
-        sys.stdout.write(
-            f'hashpic: "{args.input}" will be following hash: {hash}\n'
-            if not args.md5
-            else f"hashpic: directly given hash: {args.input}\n"
-        )
-
-    if args.c:
-        chunks = chunk_it(chunk_it(hash), 4)
-
-        for i in chunks:
-            for j in i:
-                sys.stdout.write(
-                    f"\033[38;5;{0xff - int(j, 16) if args.i else int(j, 16)}m{j}\u001b[0m"
-                )
-            sys.stdout.write("\n")
-        sys.exit(0)
-
-    colors = []
-    for i in chunk_it(hash):
-        colors.append(convert_term_to_rgb(int(i, 16)))
-
-    width = 1024
-    height = 1024
-
-    im = Image.new(mode="RGB", size=(width, height), color="#ffffff")
-    pixels = im.load()
-    for x in range(width):
-        for y in range(height):
-            if x < 0x100 and y < 0x100:
-                pixels[x, y] = colors[0]
-            elif x < 0x200 and y < 0x100:
-                pixels[x, y] = colors[1]
-            elif x < 0x300 and y < 0x100:
-                pixels[x, y] = colors[2]
-            elif x < 0x400 and y < 0x100:
-                pixels[x, y] = colors[3]
-            elif x < 0x100 and y < 0x200:
-                pixels[x, y] = colors[4]
-            elif x < 0x200 and y < 0x200:
-                pixels[x, y] = colors[5]
-            elif x < 0x300 and y < 0x200:
-                pixels[x, y] = colors[6]
-            elif x < 0x400 and y < 0x200:
-                pixels[x, y] = colors[7]
-            elif x < 0x100 and y < 0x300:
-                pixels[x, y] = colors[8]
-            elif x < 0x200 and y < 0x300:
-                pixels[x, y] = colors[9]
-            elif x < 0x300 and y < 0x300:
-                pixels[x, y] = colors[10]
-            elif x < 0x400 and y < 0x300:
-                pixels[x, y] = colors[11]
-            elif x < 0x100 and y < 0x400:
-                pixels[x, y] = colors[12]
-            elif x < 0x200 and y < 0x400:
-                pixels[x, y] = colors[13]
-            elif x < 0x300 and y < 0x400:
-                pixels[x, y] = colors[14]
-            elif x < 0x400 and y < 0x400:
-                pixels[x, y] = colors[15]
-            else:
-                pixels[x, y] = (0xFF, 0xFF, 0xFF)
-
-    if args.i:
-        im = ImageOps.invert(im)
-
-    if args.d:
-        im.show()
-
-    im.save(os.getcwd() + "/output.png")
-    sys.exit(0)
-
+        md5_mode(input=args.input, bypass=args.bypass, debug=args.d, console=args.c, invert=args.i)
 
 if __name__ == "__main__":
     main()
---
+++ hashpic/md5.py
@@ -0,0 +1,99 @@
+from PIL import Image, ImageOps
+import os
+import hashlib
+import sys
+import re
+from .util import *
+
+def md5_mode(input, bypass, debug, console, invert):
+    if not input:
+        hash = (
+            hashlib.md5(sys.stdin.read().encode()).hexdigest()
+            if not bypass
+            else sys.stdin.read().rstrip("\n").lower()
+        )
+    else:
+        hash = (
+            hashlib.md5(" ".join(input).encode()).hexdigest()
+            if not bypass
+            else input.lower()
+        )
+
+    pattern = re.compile(r"^[a-f0-9]{32}$")
+    match = pattern.match(hash)
+    if not match:
+        sys.stderr.write(f"{hash} is not a valid MD5 hash\n")
+        sys.exit(-1)
+
+    if debug:
+        sys.stdout.write(
+            f'hashpic: "{input}" will be following hash: {hash}\n'
+            if not bypass
+            else f"hashpic: directly given hash: {input}\n"
+        )
+
+    if console:
+        chunks = chunk_it(chunk_it(hash), 4)
+
+        for i in chunks:
+            for j in i:
+                sys.stdout.write(
+                    f"\033[38;5;{0xff - int(j, 16) if args.i else int(j, 16)}m{j}\u001b[0m"
+                )
+            sys.stdout.write("\n")
+        sys.exit(0)
+
+    colors = []
+    for i in chunk_it(hash):
+        colors.append(convert_term_to_rgb(int(i, 16)))
+
+    width = 1024
+    height = 1024
+
+    im = Image.new(mode="RGB", size=(width, height), color="#ffffff")
+    pixels = im.load()
+    for x in range(width):
+        for y in range(height):
+            if x < 0x100 and y < 0x100:
+                pixels[x, y] = colors[0]
+            elif x < 0x200 and y < 0x100:
+                pixels[x, y] = colors[1]
+            elif x < 0x300 and y < 0x100:
+                pixels[x, y] = colors[2]
+            elif x < 0x400 and y < 0x100:
+                pixels[x, y] = colors[3]
+            elif x < 0x100 and y < 0x200:
+                pixels[x, y] = colors[4]
+            elif x < 0x200 and y < 0x200:
+                pixels[x, y] = colors[5]
+            elif x < 0x300 and y < 0x200:
+                pixels[x, y] = colors[6]
+            elif x < 0x400 and y < 0x200:
+                pixels[x, y] = colors[7]
+            elif x < 0x100 and y < 0x300:
+                pixels[x, y] = colors[8]
+            elif x < 0x200 and y < 0x300:
+                pixels[x, y] = colors[9]
+            elif x < 0x300 and y < 0x300:
+                pixels[x, y] = colors[10]
+            elif x < 0x400 and y < 0x300:
+                pixels[x, y] = colors[11]
+            elif x < 0x100 and y < 0x400:
+                pixels[x, y] = colors[12]
+            elif x < 0x200 and y < 0x400:
+                pixels[x, y] = colors[13]
+            elif x < 0x300 and y < 0x400:
+                pixels[x, y] = colors[14]
+            elif x < 0x400 and y < 0x400:
+                pixels[x, y] = colors[15]
+            else:
+                pixels[x, y] = (0xFF, 0xFF, 0xFF)
+
+    if invert:
+        im = ImageOps.invert(im)
+
+    if debug:
+        im.show()
+
+    im.save(os.getcwd() + "/output.png")
+    sys.exit(0)
