fd60ac8883ec17837eb8d17bb6282ccd99314dd2
0xflotus <0xflotus@gmail.com>
2022-02-04 16:00:46 +0100

    added support for digest length 144

--- hashpic/shake256.py
+++ hashpic/shake256.py
@@ -9,9 +9,9 @@ def shake_256_mode(input, bypass, debug, console, invert, digest_length):
         sys.stderr.write("Please specify a --length\n")
         sys.exit(-1)
 
-    if int(digest_length) not in [16, 25, 36, 64, 100]:
+    if int(digest_length) not in [16, 25, 36, 64, 100, 144]:
         sys.stderr.write(
-            "Sorry, only a length of one of [16, 25, 36, 64, 100] is currently possible\n"
+            "Sorry, only a length of one of [16, 25, 36, 64, 100, 144] is currently possible\n"
         )
         sys.exit(-1)
 
@@ -40,6 +40,7 @@ def shake_256_mode(input, bypass, debug, console, invert, digest_length):
         36: r"^[a-f0-9]{72}$",
         64: r"^[a-f0-9]{128}$",
         100: r"^[a-f0-9]{200}$",
+        144: r"^[a-f0-9]{288}$",
     }
 
     regex_str = regex_dict[variable_digest_length]
@@ -79,7 +80,9 @@ def shake_256_mode(input, bypass, debug, console, invert, digest_length):
     im = Image.new(mode="RGB", size=(width, height), color="#ffffff")
     pixels = im.load()
 
-    if variable_digest_length == 0x64:
+    if variable_digest_length == 0x90:
+        _144(pixels, colors, (width, height))
+    elif variable_digest_length == 0x64:
         _100(pixels, colors, (width, height))
     elif variable_digest_length == 0x40:
         _64(pixels, colors, (width, height))
@@ -619,5 +622,300 @@ def _100(pixels, colors, dimension):
                 pixels[x, y] = colors[98]
             elif x < 0x4B0 and y < 0x4B0:
                 pixels[x, y] = colors[99]
+            else:
+                pixels[x, y] = (0xFF, 0xFF, 0xFF)
+
+def _144(pixels, colors, dimension):
+    (width, height) = dimension
+    for x in range(width):
+        for y in range(height):
+            if x < 0x64 and y < 0x64:
+                pixels[x, y] = colors[0]
+            elif x < 0xc8 and y < 0x64:
+                pixels[x, y] = colors[1]
+            elif x < 0x12c and y < 0x64:
+                pixels[x, y] = colors[2]
+            elif x < 0x190 and y < 0x64:
+                pixels[x, y] = colors[3]
+            elif x < 0x1f4 and y < 0x64:
+                pixels[x, y] = colors[4]
+            elif x < 0x258 and y < 0x64:
+                pixels[x, y] = colors[5]
+            elif x < 0x2bc and y < 0x64:
+                pixels[x, y] = colors[6]
+            elif x < 0x320 and y < 0x64:
+                pixels[x, y] = colors[7]
+            elif x < 0x384 and y < 0x64:
+                pixels[x, y] = colors[8]
+            elif x < 0x3e8 and y < 0x64:
+                pixels[x, y] = colors[9]
+            elif x < 0x44c and y < 0x64:
+                pixels[x, y] = colors[10]
+            elif x < 0x4b0 and y < 0x64:
+                pixels[x, y] = colors[11]
+            elif x < 0x64 and y < 0xc8:
+                pixels[x, y] = colors[12]
+            elif x < 0xc8 and y < 0xc8:
+                pixels[x, y] = colors[13]
+            elif x < 0x12c and y < 0xc8:
+                pixels[x, y] = colors[14]
+            elif x < 0x190 and y < 0xc8:
+                pixels[x, y] = colors[15]
+            elif x < 0x1f4 and y < 0xc8:
+                pixels[x, y] = colors[16]
+            elif x < 0x258 and y < 0xc8:
+                pixels[x, y] = colors[17]
+            elif x < 0x2bc and y < 0xc8:
+                pixels[x, y] = colors[18]
+            elif x < 0x320 and y < 0xc8:
+                pixels[x, y] = colors[19]
+            elif x < 0x384 and y < 0xc8:
+                pixels[x, y] = colors[20]
+            elif x < 0x3e8 and y < 0xc8:
+                pixels[x, y] = colors[21]
+            elif x < 0x44c and y < 0xc8:
+                pixels[x, y] = colors[22]
+            elif x < 0x4b0 and y < 0xc8:
+                pixels[x, y] = colors[23]
+            elif x < 0x64 and y < 0x12c:
+                pixels[x, y] = colors[24]
+            elif x < 0xc8 and y < 0x12c:
+                pixels[x, y] = colors[25]
+            elif x < 0x12c and y < 0x12c:
+                pixels[x, y] = colors[26]
+            elif x < 0x190 and y < 0x12c:
+                pixels[x, y] = colors[27]
+            elif x < 0x1f4 and y < 0x12c:
+                pixels[x, y] = colors[28]
+            elif x < 0x258 and y < 0x12c:
+                pixels[x, y] = colors[29]
+            elif x < 0x2bc and y < 0x12c:
+                pixels[x, y] = colors[30]
+            elif x < 0x320 and y < 0x12c:
+                pixels[x, y] = colors[31]
+            elif x < 0x384 and y < 0x12c:
+                pixels[x, y] = colors[32]
+            elif x < 0x3e8 and y < 0x12c:
+                pixels[x, y] = colors[33]
+            elif x < 0x44c and y < 0x12c:
+                pixels[x, y] = colors[34]
+            elif x < 0x4b0 and y < 0x12c:
+                pixels[x, y] = colors[35]
+            elif x < 0x64 and y < 0x190:
+                pixels[x, y] = colors[36]
+            elif x < 0xc8 and y < 0x190:
+                pixels[x, y] = colors[37]
+            elif x < 0x12c and y < 0x190:
+                pixels[x, y] = colors[38]
+            elif x < 0x190 and y < 0x190:
+                pixels[x, y] = colors[39]
+            elif x < 0x1f4 and y < 0x190:
+                pixels[x, y] = colors[40]
+            elif x < 0x258 and y < 0x190:
+                pixels[x, y] = colors[41]
+            elif x < 0x2bc and y < 0x190:
+                pixels[x, y] = colors[42]
+            elif x < 0x320 and y < 0x190:
+                pixels[x, y] = colors[43]
+            elif x < 0x384 and y < 0x190:
+                pixels[x, y] = colors[44]
+            elif x < 0x3e8 and y < 0x190:
+                pixels[x, y] = colors[45]
+            elif x < 0x44c and y < 0x190:
+                pixels[x, y] = colors[46]
+            elif x < 0x4b0 and y < 0x190:
+                pixels[x, y] = colors[47]
+            elif x < 0x64 and y < 0x1f4:
+                pixels[x, y] = colors[48]
+            elif x < 0xc8 and y < 0x1f4:
+                pixels[x, y] = colors[49]
+            elif x < 0x12c and y < 0x1f4:
+                pixels[x, y] = colors[50]
+            elif x < 0x190 and y < 0x1f4:
+                pixels[x, y] = colors[51]
+            elif x < 0x1f4 and y < 0x1f4:
+                pixels[x, y] = colors[52]
+            elif x < 0x258 and y < 0x1f4:
+                pixels[x, y] = colors[53]
+            elif x < 0x2bc and y < 0x1f4:
+                pixels[x, y] = colors[54]
+            elif x < 0x320 and y < 0x1f4:
+                pixels[x, y] = colors[55]
+            elif x < 0x384 and y < 0x1f4:
+                pixels[x, y] = colors[56]
+            elif x < 0x3e8 and y < 0x1f4:
+                pixels[x, y] = colors[57]
+            elif x < 0x44c and y < 0x1f4:
+                pixels[x, y] = colors[58]
+            elif x < 0x4b0 and y < 0x1f4:
+                pixels[x, y] = colors[59]
+            elif x < 0x64 and y < 0x258:
+                pixels[x, y] = colors[60]
+            elif x < 0xc8 and y < 0x258:
+                pixels[x, y] = colors[61]
+            elif x < 0x12c and y < 0x258:
+                pixels[x, y] = colors[62]
+            elif x < 0x190 and y < 0x258:
+                pixels[x, y] = colors[63]
+            elif x < 0x1f4 and y < 0x258:
+                pixels[x, y] = colors[64]
+            elif x < 0x258 and y < 0x258:
+                pixels[x, y] = colors[65]
+            elif x < 0x2bc and y < 0x258:
+                pixels[x, y] = colors[66]
+            elif x < 0x320 and y < 0x258:
+                pixels[x, y] = colors[67]
+            elif x < 0x384 and y < 0x258:
+                pixels[x, y] = colors[68]
+            elif x < 0x3e8 and y < 0x258:
+                pixels[x, y] = colors[69]
+            elif x < 0x44c and y < 0x258:
+                pixels[x, y] = colors[70]
+            elif x < 0x4b0 and y < 0x258:
+                pixels[x, y] = colors[71]
+            elif x < 0x64 and y < 0x2bc:
+                pixels[x, y] = colors[72]
+            elif x < 0xc8 and y < 0x2bc:
+                pixels[x, y] = colors[73]
+            elif x < 0x12c and y < 0x2bc:
+                pixels[x, y] = colors[74]
+            elif x < 0x190 and y < 0x2bc:
+                pixels[x, y] = colors[75]
+            elif x < 0x1f4 and y < 0x2bc:
+                pixels[x, y] = colors[76]
+            elif x < 0x258 and y < 0x2bc:
+                pixels[x, y] = colors[77]
+            elif x < 0x2bc and y < 0x2bc:
+                pixels[x, y] = colors[78]
+            elif x < 0x320 and y < 0x2bc:
+                pixels[x, y] = colors[79]
+            elif x < 0x384 and y < 0x2bc:
+                pixels[x, y] = colors[80]
+            elif x < 0x3e8 and y < 0x2bc:
+                pixels[x, y] = colors[81]
+            elif x < 0x44c and y < 0x2bc:
+                pixels[x, y] = colors[82]
+            elif x < 0x4b0 and y < 0x2bc:
+                pixels[x, y] = colors[83]
+            elif x < 0x64 and y < 0x320:
+                pixels[x, y] = colors[84]
+            elif x < 0xc8 and y < 0x320:
+                pixels[x, y] = colors[85]
+            elif x < 0x12c and y < 0x320:
+                pixels[x, y] = colors[86]
+            elif x < 0x190 and y < 0x320:
+                pixels[x, y] = colors[87]
+            elif x < 0x1f4 and y < 0x320:
+                pixels[x, y] = colors[88]
+            elif x < 0x258 and y < 0x320:
+                pixels[x, y] = colors[89]
+            elif x < 0x2bc and y < 0x320:
+                pixels[x, y] = colors[90]
+            elif x < 0x320 and y < 0x320:
+                pixels[x, y] = colors[91]
+            elif x < 0x384 and y < 0x320:
+                pixels[x, y] = colors[92]
+            elif x < 0x3e8 and y < 0x320:
+                pixels[x, y] = colors[93]
+            elif x < 0x44c and y < 0x320:
+                pixels[x, y] = colors[94]
+            elif x < 0x4b0 and y < 0x320:
+                pixels[x, y] = colors[95]
+            elif x < 0x64 and y < 0x384:
+                pixels[x, y] = colors[96]
+            elif x < 0xc8 and y < 0x384:
+                pixels[x, y] = colors[97]
+            elif x < 0x12c and y < 0x384:
+                pixels[x, y] = colors[98]
+            elif x < 0x190 and y < 0x384:
+                pixels[x, y] = colors[99]
+            elif x < 0x1f4 and y < 0x384:
+                pixels[x, y] = colors[100]
+            elif x < 0x258 and y < 0x384:
+                pixels[x, y] = colors[101]
+            elif x < 0x2bc and y < 0x384:
+                pixels[x, y] = colors[102]
+            elif x < 0x320 and y < 0x384:
+                pixels[x, y] = colors[103]
+            elif x < 0x384 and y < 0x384:
+                pixels[x, y] = colors[104]
+            elif x < 0x3e8 and y < 0x384:
+                pixels[x, y] = colors[105]
+            elif x < 0x44c and y < 0x384:
+                pixels[x, y] = colors[106]
+            elif x < 0x4b0 and y < 0x384:
+                pixels[x, y] = colors[107]
+            elif x < 0x64 and y < 0x3e8:
+                pixels[x, y] = colors[108]
+            elif x < 0xc8 and y < 0x3e8:
+                pixels[x, y] = colors[109]
+            elif x < 0x12c and y < 0x3e8:
+                pixels[x, y] = colors[110]
+            elif x < 0x190 and y < 0x3e8:
+                pixels[x, y] = colors[111]
+            elif x < 0x1f4 and y < 0x3e8:
+                pixels[x, y] = colors[112]
+            elif x < 0x258 and y < 0x3e8:
+                pixels[x, y] = colors[113]
+            elif x < 0x2bc and y < 0x3e8:
+                pixels[x, y] = colors[114]
+            elif x < 0x320 and y < 0x3e8:
+                pixels[x, y] = colors[115]
+            elif x < 0x384 and y < 0x3e8:
+                pixels[x, y] = colors[116]
+            elif x < 0x3e8 and y < 0x3e8:
+                pixels[x, y] = colors[117]
+            elif x < 0x44c and y < 0x3e8:
+                pixels[x, y] = colors[118]
+            elif x < 0x4b0 and y < 0x3e8:
+                pixels[x, y] = colors[119]
+            elif x < 0x64 and y < 0x44c:
+                pixels[x, y] = colors[120]
+            elif x < 0xc8 and y < 0x44c:
+                pixels[x, y] = colors[121]
+            elif x < 0x12c and y < 0x44c:
+                pixels[x, y] = colors[122]
+            elif x < 0x190 and y < 0x44c:
+                pixels[x, y] = colors[123]
+            elif x < 0x1f4 and y < 0x44c:
+                pixels[x, y] = colors[124]
+            elif x < 0x258 and y < 0x44c:
+                pixels[x, y] = colors[125]
+            elif x < 0x2bc and y < 0x44c:
+                pixels[x, y] = colors[126]
+            elif x < 0x320 and y < 0x44c:
+                pixels[x, y] = colors[127]
+            elif x < 0x384 and y < 0x44c:
+                pixels[x, y] = colors[128]
+            elif x < 0x3e8 and y < 0x44c:
+                pixels[x, y] = colors[129]
+            elif x < 0x44c and y < 0x44c:
+                pixels[x, y] = colors[130]
+            elif x < 0x4b0 and y < 0x44c:
+                pixels[x, y] = colors[131]
+            elif x < 0x64 and y < 0x4b0:
+                pixels[x, y] = colors[132]
+            elif x < 0xc8 and y < 0x4b0:
+                pixels[x, y] = colors[133]
+            elif x < 0x12c and y < 0x4b0:
+                pixels[x, y] = colors[134]
+            elif x < 0x190 and y < 0x4b0:
+                pixels[x, y] = colors[135]
+            elif x < 0x1f4 and y < 0x4b0:
+                pixels[x, y] = colors[136]
+            elif x < 0x258 and y < 0x4b0:
+                pixels[x, y] = colors[137]
+            elif x < 0x2bc and y < 0x4b0:
+                pixels[x, y] = colors[138]
+            elif x < 0x320 and y < 0x4b0:
+                pixels[x, y] = colors[139]
+            elif x < 0x384 and y < 0x4b0:
+                pixels[x, y] = colors[140]
+            elif x < 0x3e8 and y < 0x4b0:
+                pixels[x, y] = colors[141]
+            elif x < 0x44c and y < 0x4b0:
+                pixels[x, y] = colors[142]
+            elif x < 0x4b0 and y < 0x4b0:
+                pixels[x, y] = colors[143]
             else:
                 pixels[x, y] = (0xFF, 0xFF, 0xFF)
