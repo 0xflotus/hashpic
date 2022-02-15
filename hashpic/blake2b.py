a0a920239750c06d3c9e724a8bc65e1046f69fbf
0xflotus <0xflotus@gmail.com>
2022-02-15 23:12:41 +0100

    blake2b performance boost from 1,31s to 0,2s

--- hashpic/blake2b.py
+++ hashpic/blake2b.py
@@ -1,7 +1,6 @@
 import sys, hashlib, os
-from PIL import Image, ImageOps
+from PIL import Image, ImageOps, ImageDraw
 from .util import *
-from tqdm import tqdm
 
 
 def blake2b_mode(
@@ -58,139 +57,17 @@ def blake2b_mode(
     width, height = 1024, 1024
 
     im = Image.new(mode="RGB", size=(width, height), color="#ffffff")
-    pixels = im.load()
-    for x in tqdm(range(width)):
-        for y in range(height):
-            if x < 0x80 and y < 0x80:
-                pixels[x, y] = colors[0]
-            elif x < 0x100 and y < 0x80:
-                pixels[x, y] = colors[1]
-            elif x < 0x180 and y < 0x80:
-                pixels[x, y] = colors[2]
-            elif x < 0x200 and y < 0x80:
-                pixels[x, y] = colors[3]
-            elif x < 0x280 and y < 0x80:
-                pixels[x, y] = colors[4]
-            elif x < 0x300 and y < 0x80:
-                pixels[x, y] = colors[5]
-            elif x < 0x380 and y < 0x80:
-                pixels[x, y] = colors[6]
-            elif x < 0x400 and y < 0x80:
-                pixels[x, y] = colors[7]
-            elif x < 0x80 and y < 0x100:
-                pixels[x, y] = colors[8]
-            elif x < 0x100 and y < 0x100:
-                pixels[x, y] = colors[9]
-            elif x < 0x180 and y < 0x100:
-                pixels[x, y] = colors[10]
-            elif x < 0x200 and y < 0x100:
-                pixels[x, y] = colors[11]
-            elif x < 0x280 and y < 0x100:
-                pixels[x, y] = colors[12]
-            elif x < 0x300 and y < 0x100:
-                pixels[x, y] = colors[13]
-            elif x < 0x380 and y < 0x100:
-                pixels[x, y] = colors[14]
-            elif x < 0x400 and y < 0x100:
-                pixels[x, y] = colors[15]
-            elif x < 0x80 and y < 0x180:
-                pixels[x, y] = colors[16]
-            elif x < 0x100 and y < 0x180:
-                pixels[x, y] = colors[17]
-            elif x < 0x180 and y < 0x180:
-                pixels[x, y] = colors[18]
-            elif x < 0x200 and y < 0x180:
-                pixels[x, y] = colors[19]
-            elif x < 0x280 and y < 0x180:
-                pixels[x, y] = colors[20]
-            elif x < 0x300 and y < 0x180:
-                pixels[x, y] = colors[21]
-            elif x < 0x380 and y < 0x180:
-                pixels[x, y] = colors[22]
-            elif x < 0x400 and y < 0x180:
-                pixels[x, y] = colors[23]
-            elif x < 0x80 and y < 0x200:
-                pixels[x, y] = colors[24]
-            elif x < 0x100 and y < 0x200:
-                pixels[x, y] = colors[25]
-            elif x < 0x180 and y < 0x200:
-                pixels[x, y] = colors[26]
-            elif x < 0x200 and y < 0x200:
-                pixels[x, y] = colors[27]
-            elif x < 0x280 and y < 0x200:
-                pixels[x, y] = colors[28]
-            elif x < 0x300 and y < 0x200:
-                pixels[x, y] = colors[29]
-            elif x < 0x380 and y < 0x200:
-                pixels[x, y] = colors[30]
-            elif x < 0x400 and y < 0x200:
-                pixels[x, y] = colors[31]
-            elif x < 0x80 and y < 0x280:
-                pixels[x, y] = colors[32]
-            elif x < 0x100 and y < 0x280:
-                pixels[x, y] = colors[33]
-            elif x < 0x180 and y < 0x280:
-                pixels[x, y] = colors[34]
-            elif x < 0x200 and y < 0x280:
-                pixels[x, y] = colors[35]
-            elif x < 0x280 and y < 0x280:
-                pixels[x, y] = colors[36]
-            elif x < 0x300 and y < 0x280:
-                pixels[x, y] = colors[37]
-            elif x < 0x380 and y < 0x280:
-                pixels[x, y] = colors[38]
-            elif x < 0x400 and y < 0x280:
-                pixels[x, y] = colors[39]
-            elif x < 0x80 and y < 0x300:
-                pixels[x, y] = colors[40]
-            elif x < 0x100 and y < 0x300:
-                pixels[x, y] = colors[41]
-            elif x < 0x180 and y < 0x300:
-                pixels[x, y] = colors[42]
-            elif x < 0x200 and y < 0x300:
-                pixels[x, y] = colors[43]
-            elif x < 0x280 and y < 0x300:
-                pixels[x, y] = colors[44]
-            elif x < 0x300 and y < 0x300:
-                pixels[x, y] = colors[45]
-            elif x < 0x380 and y < 0x300:
-                pixels[x, y] = colors[46]
-            elif x < 0x400 and y < 0x300:
-                pixels[x, y] = colors[47]
-            elif x < 0x80 and y < 0x380:
-                pixels[x, y] = colors[48]
-            elif x < 0x100 and y < 0x380:
-                pixels[x, y] = colors[49]
-            elif x < 0x180 and y < 0x380:
-                pixels[x, y] = colors[50]
-            elif x < 0x200 and y < 0x380:
-                pixels[x, y] = colors[51]
-            elif x < 0x280 and y < 0x380:
-                pixels[x, y] = colors[52]
-            elif x < 0x300 and y < 0x380:
-                pixels[x, y] = colors[53]
-            elif x < 0x380 and y < 0x380:
-                pixels[x, y] = colors[54]
-            elif x < 0x400 and y < 0x380:
-                pixels[x, y] = colors[55]
-            elif x < 0x80 and y < 0x400:
-                pixels[x, y] = colors[56]
-            elif x < 0x100 and y < 0x400:
-                pixels[x, y] = colors[57]
-            elif x < 0x180 and y < 0x400:
-                pixels[x, y] = colors[58]
-            elif x < 0x200 and y < 0x400:
-                pixels[x, y] = colors[59]
-            elif x < 0x280 and y < 0x400:
-                pixels[x, y] = colors[60]
-            elif x < 0x300 and y < 0x400:
-                pixels[x, y] = colors[61]
-            elif x < 0x380 and y < 0x400:
-                pixels[x, y] = colors[62]
-            elif x < 0x400 and y < 0x400:
-                pixels[x, y] = colors[63]
-            else:
-                pixels[x, y] = (0xFF, 0xFF, 0xFF)
+    draw = ImageDraw.Draw(im)
+    m_size = int((len(hash) // 2) ** 0.5)
+    steps = int(width // m_size)
+    store = [
+        (i, steps * (x + 1), i + steps, steps * x)
+        for x in range(m_size)
+        for i in range(0, width, steps)
+    ]
+
+    for idx, elem in enumerate(store):
+        draw.rectangle(elem, fill=colors[idx])
 
     if invert:
         im = ImageOps.invert(im)
